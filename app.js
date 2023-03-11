d3bfcda6a222ea611a31920529d6dc957516c793
Amir Ali anwar <amiralianwar611@gmail.com>
2023-03-11 14:37:16 +0500

    connectDB function modification

--- app.js
+++ app.js
@@ -7,13 +7,11 @@ const PORT = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URL)
-    app.listen(() => {
-      console.log(`Server listening on the Port ${PORT}`);
-    })
+    await connectDB(process.env.MONGO_URL);
+    app.listen(PORT, console.log(`server listening on the port ${PORT}`));
   } catch (error) {
     console.log(error);
   }
-}
+};
 
 start()
--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,16 @@
 const mongoose = require('mongoose');
 
 const connectDB = (url) => {
-  return mongoose.connect(url);
+  try {
+      mongoose.set("strictQuery", false);
+      mongoose.connect(url, () => {
+      console.log("MongoDB connected");
+    });
+
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
