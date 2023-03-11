f5ae3fcd5b4487a173f85922d3d37506a75fdbb0
Amir Ali anwar <amiralianwar611@gmail.com>
2023-03-11 14:27:58 +0500

    Setup the app.js file

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('E-Commerce API');
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./db/connect');
+const app = express()
+const PORT = process.env.PORT || 5000;
+
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL)
+    app.listen(() => {
+      console.log(`Server listening on the Port ${PORT}`);
+    })
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start()
