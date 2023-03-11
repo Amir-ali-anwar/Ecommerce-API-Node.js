4ffd874e2a72849c491dafd5d860bfd27760cb62
Amir Ali anwar <amiralianwar611@gmail.com>
2023-03-11 17:09:07 +0500

    register user completed

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,21 +1,33 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
-
+const CustomAPIError = require("../errors");
 const register = async (req, res) => {
-    res.send('register')
+  const { name, email, password } = req.body
+  if (!name || !email || !password) {
+    throw new CustomAPIError.BadRequestError("please provide all the values")
+  }
+  const alreadyExist = await User.findOne({ email })
+  if (alreadyExist) {
+    throw new CustomAPIError.BadRequestError("Email already exist")
+  }
+  const isFirstUser = await User.countDocuments({}) === 0;
+  const role = isFirstUser ? "admin" : "user";
+  const user = await User.create({ name, email, password, role })
+  res.status(StatusCodes.CREATED).json({ user })
+
 }
 const login = async (req, res) => {
-    res.send('login')
+  res.send('login')
 }
 const logout = async (req, res) => {
-    res.send('logout')
+  res.send('logout')
 }
 
 
 
 
 module.exports = {
-    register,
-    login,
-    logout,
+  register,
+  login,
+  logout,
 };
