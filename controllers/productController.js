088d2f2aa1921551c6cad8c1156c4a824aecde7f
Amir Ali anwar <amiralianwar611@gmail.com>
2023-03-18 11:37:50 +0500

    added create product, single product and getall product controllers

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,27 +1,36 @@
 const { StatusCodes } = require("http-status-codes");
 const CustomAPIError = require("../errors");
-const Product =require('../models/Prouducts')
-const { attachCookiesToResponse,createUserToken } = require('../utils/index')
+const Product = require('../models/Prouducts')
+const { attachCookiesToResponse, createUserToken } = require('../utils/index')
 
-const createProduct= async(res,req)=>{
-    res.send('create Product')
+const createProduct = async (req, res) => {
+    req.body.user = req.user.UserId
+    const product = await Product.create(req.body)
+    res.status(StatusCodes.CREATED).json({ product })
 }
-const getAllProduct= async(res,req)=>{
-    res.send('get All Product')
+const getAllProduct = async (req, res) => {
+    const product = await Product.find({})
+    res.status(StatusCodes.OK).json({ product, nbHits:product.length})
+
 }
-const getSingleProduct= async(res,req)=>{
-    res.send('getSingleProduct')
+const getSingleProduct = async (req, res) => {
+    const id = req.params.id;
+    const product = await Product.findById({ _id: id })
+    if (!product) {
+        throw new CustomAPIError.NotFoundError("Product not found with the id")
+    }
+    res.status(StatusCodes.OK).json({ product, nbHits: product.length })
 }
-const updateProduct= async(res,req)=>{
+const updateProduct = async (req, res) => {
     res.send('updateProduct')
 }
-const deleteProduct= async(res,req)=>{
+const deleteProduct = async (req, res) => {
     res.send('deleteProduct')
 }
-const uploadImage= async(res,req)=>{
+const uploadImage = async (req, res) => {
     res.send('uploadImage')
 }
-module.exports={
+module.exports = {
     createProduct,
     getAllProduct,
     getSingleProduct,
