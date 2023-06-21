f22241fe4105f39558829b042c4aaa59db3f1a75
Amir Ali anwar <amiralianwar611@gmail.com>
2023-06-21 18:06:20 +0500

    updated the package.json

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const router= express.Router();
+const 
+router= express.Router();
 const { createOrders, getAllOrders, getSingeOrder, getCurentUserOrders, updateOrder}=require('../controllers/orderController')
 const {
     authenticateUser,
